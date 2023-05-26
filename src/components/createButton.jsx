330b6ba2f976d574dc50bf3df0ff53ce4e702af3
QaziWaqasAhmad <88557951+QaziWaqasAhmad@users.noreply.github.com>
2023-05-27 01:18:03 +0500

    button work

---
+++ src/components/createButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const createButton = () => {
+  return (
+    <div>
+      <div className="create-button">
+        Click
+      </div>
+    </div>
+  )
+}
+
+export default createButton
