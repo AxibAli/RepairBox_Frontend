d2891d080201d2f05c8c453bf8bc47cea3b8ed49
Sharjeel-1999 <67510731+Sharjeel-1999@users.noreply.github.com>
2023-06-14 14:12:20 -0700

    UserDesign Pages & API Integrate
    Three DropDown APi INTegrate

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ import Col from 'react-bootstrap/Col';
 function Footer() {
   return (
     
+<<<<<<< Updated upstream
       <Row style={{backgroundColor:'#111827'}}>
         <Col>
         <div >
@@ -23,6 +24,35 @@ function Footer() {
         <p  style={{fontWeight:600,fontSize:'16px', color:'#fff',paddingLeft:50,paddingTop:5,}}>Terms and conditions</p>
         <p  style={{fontWeight:600,fontSize:'16px', color:'#fff',paddingLeft:50,paddingTop:5,}}>Privacy policy</p>
         </div>
+=======
+   <>
+    <Row xs="auto" style={{display:'flex',flexWrap:'wrap',color:'grey',backgroundColor:'#f8faff',fontSize:'18px',fontWeight:400,justifyContent:'space-around',alignItems:'center',marginTop:'24px',paddingBottom:'50px',paddingTop:"50px"}}>
+      <Col> <ul>
+        <li style={{fontSize:'26px',fontWeight:400}}><h3>START.LY</h3></li>
+        <li>Start.ly is a SASS software landing page <br/>  template.</li>
+        <a> <li style={{color:'blue'}} className='Purchase'> Purchase Now  </li></a>
+        </ul>  </Col>
+      <Col>
+      <ul>
+        <li>Portfolio</li>
+        <li>About us</li>
+        <li>Services</li>
+        <li>Contact</li>
+       
+      </ul>
+      </Col>
+      <Col>
+      <ul>
+        <li>Terms</li>
+        <li>Privacy</li>
+       
+      </ul>
+      </Col>
+      <Col> <ul>
+        <li>Facebook</li>
+        <li>Twitter</li>
+        <li>Linkedin</li>
+>>>>>>> Stashed changes
         
         </Col>
       </Row>
