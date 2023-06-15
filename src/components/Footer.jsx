ef86484e19d6ff79d3043efe378c0c2a59e3e93e
Sharjeel-1999 <67510731+Sharjeel-1999@users.noreply.github.com>
2023-06-15 07:07:26 -0700

    Users Pages Updated
    All UI are Set and Done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ import '../App.css'
 
 function Footer() {
   return (
+<<<<<<< Updated upstream
     
    <>
     <Row xs="auto" style={{display:'flex',color:'grey',backgroundColor:'#f8faff',fontSize:'18px',fontWeight:400,justifyContent:'space-around',alignItems:'center',marginTop:'24px',paddingBottom:'50px',paddingTop:"50px"}}>
@@ -32,15 +33,48 @@ function Footer() {
         <li>Facebook</li>
         <li>Twitter</li>
         <li>Linkedin</li>
+=======
+    <div >
+      <Row style={{ display: 'flex', flexWrap: 'wrap', color: 'grey', backgroundColor: '#f8faff', fontSize: '18px', fontWeight: 400,paddingLeft:100, padding: 50, marginTop: "50px" }}>
+        <Col> <ul>
+          <li style={{ fontSize: '26px', fontWeight: 400 }}><h3>START.LY</h3></li>
+          <li>Start.ly is a SASS software landing page <br />  template.</li>
+          <a> <li style={{ color: 'blue' }} className='Purchase'> Purchase Now  </li></a>
+        </ul>  </Col>
+        <Col>
+          <ul>
+            <li>Portfolio</li>
+            <li>About us</li>
+            <li>Services</li>
+            <li>Contact</li>
+
+          </ul>
+        </Col>
+        <Col>
+          <ul>
+            <li>Terms</li>
+            <li>Privacy</li>
+
+          </ul>
+        </Col>
+        <Col> <ul>
+          <li>Facebook</li>
+          <li>Twitter</li>
+          <li>Linkedin</li>
+
+
+
+        </ul></Col>
+        {/* <Col> 
+>>>>>>> Stashed changes
         
-       
-      </ul></Col>
-    </Row>
-    <Row xs="auto" style={{display:'flex',color:'grey',backgroundColor:'#f8faff',fontSize:'18px',fontWeight:400,justifyContent:'space-around',alignItems:'center',paddingBottom:'50px',paddingTop:"50px"}}>
-      <p>Copyright © 2023 All rights reserved. Start.ly</p>
-    </Row>
-    </>
-   
+        </Col> */}
+      </Row>
+      <Row xs="auto" style={{ display: 'flex', color: 'grey', backgroundColor: '#f8faff', fontSize: '18px', fontWeight: 400, justifyContent: 'space-around', alignItems: 'center', paddingBottom: '50px', paddingTop: "50px" }}>
+        <p>Copyright © 2023 All rights reserved. Start.ly</p>
+      </Row>
+    </div>
+
   );
 }
 
--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -1,12 +1,40 @@
 import React from "react";
 import Footer from "./Footer";
 
+<<<<<<< Updated upstream
 const TrackPage=()=>{
     return(
         <div>
         {/* <h1>Track Page</h1> */}
         <Footer/>
         </div>
+=======
+// const TrackPage=()=>{
+//     return(
+//         <>
+//         <h1>Track Page</h1>
+//         </>
+
+const TrackPage = () => {
+    return (
+        <div>
+            {/* <h1>Track Page</h1> */}
+            <div style={{ backgroundColor: '#fde047', width: '97vw', borderRadius: '20px', marginLeft: 20, height: '300px', }}>
+                <div>
+                    <h1 style={{ fontSize: "30px", padding: '10px', fontFamily: 'sans-serif', height: '30px', width: '568px', fontWeight: "bolder", lineHeight: 2 }}>Want to track repair order ?</h1>
+                    <p style={{ fontSize: "18px", padding: '10px', fontFamily: 'sans-serif', fontWeight: "500", marginTop: 30, color: '#4B5563' }}>Please enter your invoice tracking identity number to trace your repair order status.</p>
+
+                </div>
+                <div >
+                <label style={{ paddingLeft: 10, marginTop: 20, marginLeft: 15, fontSize: '16px', fontWeight: '600' }} >ENTER TRACK ID</label><br />
+                <input style={{ width: '95vw', height: 50, padding: 20, marginLeft: 15, border: "1px solid grey", borderRadius: '10px' }} name='ENTER TRACK ID' type="integer" placeholder="ENTER TRACK ID" /> <br />
+                <button style={{ backgroundColor: '#6b7280',marginLeft:550, borderRadius: 10,display:'flex',justifyContent:'center',alignItems:'center', marginTop: 12, color: "#fff", border: "1px solid blue", padding: "8px 16px", fontSize: '18px', fontWeight: 500, }}>TRACK YOUR ORDER STATUS</button>
+                </div>
+            </div>
+            <Footer />
+        </div>
+        
+>>>>>>> Stashed changes
     );
 }
 export default TrackPage;
