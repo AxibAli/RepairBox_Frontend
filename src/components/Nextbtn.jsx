f95f0540d921552f0f92e8eb9cda877e7b96098d
Sharjeel-1999 <67510731+Sharjeel-1999@users.noreply.github.com>
2023-06-18 15:51:35 -0700

    Priority Level Work and Information Work

--- src/components/Nextbtn.jsx
+++ src/components/Nextbtn.jsx
@@ -3,16 +3,16 @@ import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 // import Stepper from './step';
 
-function Nextbtn(props) {
+function Nextbtn({n,t}) {
   return (
     <Navbar style={{backgroundColor:'#d1d5db',padding:15,width:'80%',marginLeft:100,marginTop:200,borderRadius:5}}>
       <Container>
-      <Button style={{backgroundColor:'black',fontweight: 700,}}  variant="secondary" size="lg">
+      <Button onClick={t} style={{backgroundColor:'black',fontweight: 700,}}  variant="secondary" size="lg">
           PREVIOUS
         </Button>
       
         <Navbar.Collapse className="justify-content-end">
-        <Button onClick={props.n} style={{backgroundColor:'black',fontweight: 700,}}   variant="secondary" size="lg">
+        <Button onClick={n} style={{backgroundColor:'black',fontweight: 700,}}   variant="secondary" size="lg">
           NEXT
         </Button>
         </Navbar.Collapse>
--- src/components/step.jsx
+++ src/components/step.jsx
@@ -1,61 +1,58 @@
-// import React, { useState } from 'react';
-// import {  Steps } from 'antd';
-
-// const Step= () => {
-//   const [current, setCurrent] = useState(0);
-//   // const items.style={{}}
-
-//   const onChange = (value) => {
-//     console.log('onChange:', value);
-//     setCurrent(value);
-//   };
-//   return (
-//     <div  style={{width:'100%',paddingLeft:200}}>
-//       <Steps 
-//         className='StepsCss'
-//         current={current}
-//         onChange={onChange}
-//         items={[
-//           {
-//             title: '-',
-//             description:'REPAIR DEVICE',
-//           },
-//           {
-//             title: '-',
-//              description:'PRIORITY LEVEL',
-
-//           },
-//           {
-//             title: '-',
-//              description:'INFORMATION & PAYMENT',
-
-//           },
-//         ]}
-//       />
-      
-//     </div>
-//   );
-// };
-
-// export default Step;
 import React, { useState } from "react";
 import MultipleSelect from "./dropdown";
+import Information from "./Information";
+import Priority from "./Priority";
 import Nextbtn from "./Nextbtn";
-import '.././App.css';
 import { TiTick } from "react-icons/ti";
+
 const Stepper = () => {
-  const steps = ["REPAIR DEVICE", "PRIORITY LEVEL", "INFORMATION & PAYMENT" ];
+  const steps = ["REPAIR DEVICE", "PRIORITY LEVEL", "INFORMATION & PAYMENT"];
   const [currentStep, setCurrentStep] = useState(1);
   const [complete, setComplete] = useState(false);
-const NextBtn=() => {
-  currentStep === steps.length
-    ? setComplete(true)
-    : setCurrentStep((prev) => prev + 1);
-}
+  const [currentComponent, setCurrentComponent] = useState("MultipleSelect");
+
+  const NextBtn = () => {
+    if (currentStep === steps.length) {
+      setComplete(true);
+    } else {
+      setCurrentStep((prev) => prev + 1);
+      if (currentComponent === "MultipleSelect") {
+        setCurrentComponent("Priority");
+      } else if (currentComponent === "Priority") {
+        setCurrentComponent("Information");
+      }
+    }
+  };
+  const PrevBtn = () => {
+    if (currentStep === 1) {
+      setCurrentStep(steps.length);
+    } else {
+      setCurrentStep((prev) => prev - 1);
+    }
+  
+    if (currentComponent === "Priority") {
+      setCurrentComponent("MultipleSelect");
+    } else if (currentComponent === "Information") {
+      setCurrentComponent("Priority");
+    }
+  };
+
+  const renderComponent = () => {
+    switch (currentComponent) {
+      case "MultipleSelect":
+        return <MultipleSelect />;
+      case "Priority":
+        return <Priority />;
+      case "Information":
+        return <Information />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <>
       <div className="flex justify-between">
-
         {steps?.map((step, i) => (
           <div
             key={i}
@@ -70,18 +67,114 @@ const NextBtn=() => {
           </div>
         ))}
       </div>
-<MultipleSelect/>
-      {!complete && (
-        <Nextbtn n={NextBtn}/>
-        // <button
-        //   className="btn"
-        
-        // >
-        //   {currentStep === steps.length ? "Finish" : "Next"}
-        // </button>
-      )}
+      {renderComponent()}
+      {!complete && <Nextbtn t={PrevBtn} n={NextBtn} />}
     </>
   );
 };
 
 export default Stepper;
+
+// import React, { useState } from "react";
+// import MultipleSelect from "./dropdown";
+// import Information from "./Information";
+// import Priority from "./Priority";
+// import Nextbtn from "./Nextbtn";
+// import '.././App.css';
+// import { TiTick } from "react-icons/ti";
+// const Stepper = () => {
+//   const steps = ["REPAIR DEVICE", "PRIORITY LEVEL", "INFORMATION & PAYMENT" ];
+//   const [currentStep, setCurrentStep] = useState(1);
+//   const [complete, setComplete] = useState(false);
+
+// const NextBtn=() => {
+//   currentStep === steps.length
+//     ? setComplete(true)
+//     : setCurrentStep((prev) => prev + 1);
+   
+// }
+
+
+
+//   return (
+//     <>
+//       <div className="flex justify-between">
+
+//         {steps?.map((step, i) => (
+//           <div
+//             key={i}
+//             className={`step-item ${currentStep === i + 1 && "active"} ${
+//               (i + 1 < currentStep || complete) && "complete"
+//             } `}
+//           >
+//             <div className="step">
+//               {i + 1 < currentStep || complete ? <TiTick size={24} /> : i + 1}
+//             </div>
+//             <p className="text-gray-500">{step}</p>
+//           </div>
+//         ))}
+//       </div>
+//       <MultipleSelect/>
+      
+//       {/* <Priority/> */}
+//       {/* <Information/> */}
+//       {!complete && (
+//         <Nextbtn n={NextBtn}/>
+       
+//       )}
+//     </>
+//   );
+// };
+
+// export default Stepper;
+// import React, { useState } from "react";
+// import MultipleSelect from "./dropdown";
+// import Information from "./Information";
+// import Priority from "./Priority";
+// import Nextbtn from "./Nextbtn";
+// import { TiTick } from "react-icons/ti";
+
+// const Stepper = () => {
+//   const steps = ["REPAIR DEVICE", "PRIORITY LEVEL", "INFORMATION & PAYMENT"];
+//   const [currentStep, setCurrentStep] = useState(1);
+//   const [complete, setComplete] = useState(false);
+
+//   const NextBtn = () => {
+//     if (currentStep === steps.length) {
+//       setComplete(true);
+//     } else {
+//       setCurrentStep((prev) => prev + 1);
+//     }
+//   };
+
+//   return (
+//     <>
+//       <div className="flex justify-between">
+//         {steps?.map((step, i) => (
+//           <div
+//             key={i}
+//             className={`step-item ${currentStep === i + 1 && "active"} ${
+//               (i + 1 < currentStep || complete) && "complete"
+//             } `}
+//           >
+//             <div className="step">
+//               {i + 1 < currentStep || complete ? <TiTick size={24} /> : i + 1}
+//             </div>
+//             <p className="text-gray-500">{step}</p>
+//           </div>
+//         ))}
+//       </div>
+//       {currentStep === 2 ? (
+//         <Priority />
+//       ) : (
+//         <>
+//           <MultipleSelect />
+//           {/* <Information /> */}
+//         </>
+//       )}
+//       {!complete && <Nextbtn n={NextBtn} />}
+//     </>
+//   );
+// };
+
+// export default Stepper;
