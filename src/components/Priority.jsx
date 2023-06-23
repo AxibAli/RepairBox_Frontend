5d91112fbef1b38eff7ade7fe9a6ba1a1724e146
Sharjeel-1999 <67510731+Sharjeel-1999@users.noreply.github.com>
2023-06-23 08:51:09 -0700

    PriorityAPiWorking

--- src/components/Priority.jsx
+++ src/components/Priority.jsx
@@ -1,67 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Stack from 'react-bootstrap/Stack';
 import Container from 'react-bootstrap/Container';
 import '../App.css';
 import Information from './Information';
+import axios from "axios";
 
-function Priority({setSelectedPriority}) {
+function Priority({ setSelectedPriority }) {
 
+
+  const [showData, SetshowData] = React.useState([]);
   const handlePriorityChange = (event) => {
     setSelectedPriority(event.target.value);
   };
+  useEffect(() => {
+    FetchApiData();
+  }, [])
+  const FetchApiData = async () => {
+    try {
+      const response = await axios.get("http://18.221.148.248:84/api/v1/Order/GetPriorities");
 
+      SetshowData(response.data.data);
+
+
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  console.log(showData, "showData")
   return (
     <Container>
       <h2>Select Priority</h2>
       <p>Repair processing priority.</p>
-      <Stack className='PriorityCss' direction="horizontal" gap={3}>
-        <div className="p-2">
-          <input
-            type="radio"
-            name="myRadio"
-            value="$0.00"
-            onChange={handlePriorityChange}
-          />
-        </div>
-        <div className="p-2">Urgent</div>
-        <div className="p-2 ms-auto">$0.00</div>
-      </Stack>
-      <Stack className='PriorityCss' direction="horizontal" gap={3}>
-        <div className="p-2">
-          <input
-            type="radio"
-            name="myRadio"
-            value="$1.00"
-            onChange={handlePriorityChange}
-          />
-        </div>
-        <div className="p-2">Low</div>
-        <div className="p-2 ms-auto">$1.00</div>
-      </Stack>
-      <Stack className='PriorityCss' direction="horizontal" gap={3}>
-        <div className="p-2">
-          <input
-            type="radio"
-            name="myRadio"
-            value="$2.00"
-            onChange={handlePriorityChange}
-          />
-        </div>
-        <div className="p-2">Medium</div>
-        <div className="p-2 ms-auto">$2.00</div>
-      </Stack>
-      <Stack className='PriorityCss' direction="horizontal" gap={3}>
-        <div className="p-2">
-          <input
-            type="radio"
-            name="myRadio"
-            value="$5.00"
-            onChange={handlePriorityChange}
-          />
-        </div>
-        <div className="p-2">High</div>
-        <div className="p-2 ms-auto">$5.00</div>
-      </Stack>
+      {showData && showData.map((item, value) => (
+
+        <Stack className='PriorityCss' direction="horizontal" gap={3}>
+          <div className="p-2">
+            <input
+              type="radio"
+              name="myRadio"
+              // value={id}
+              key={value}
+              onChange={handlePriorityChange}
+            />
+          </div>
+          <div className="p-2">{item.name}</div>
+          <div className="p-2 ms-auto">${item.processCharges}</div>
+        </Stack>
+      ))}
     </Container>
   );
 }
