9eec76defffe033d7bd4651a735c15565172227a
Muhammad Azib Ali <muhammadazibali20@gmail.com>
2023-07-04 07:39:18 +0500

    login page finlized mobile responsive as well

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,28 +1,76 @@
-import React from "react";
-import Loginlogo from '../images/logohome.png'
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import LoginLogo from '../images/logohome.png';
+import '../App.css';
 
-import '../App.css'
+const WorkshopPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [logoLoaded, setLogoLoaded] = useState(false);
 
-const Workshopage=()=>{
-    return(
-        <div style={{backgroundColor:'#edf8f3',display:'flex',justifyContent:'center',alignItems:'center',height:'100vh',width:"100vw"}}>
-{/* 
-        <h1>Workshopage Page</h1> */}
-        <div style={{backgroundColor:'#ffffff',height:'650px',width:"500px",borderRadius:'20px'}}>
-            <div  style={{display:'flex',justifyContent:'center',alignItems:'center',paddingTop:'20px'}}>
-            <img src={Loginlogo} style={{height:'100px',width:'100px',}}/>
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleLogoLoad = () => {
+    setLogoLoaded(true);
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white rounded-lg shadow-lg p-8 w-80">
+        <div className="flex justify-center items-center">
+          {!logoLoaded && (
+            <div className="h-24 w-24 flex items-center justify-center">
+              <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-500"></div>
             </div>
-            <div style={{margin:20,padding:20}} >
-     <label style={{paddingLeft:10,fontSize:'20px',fontWeight:'400'}} >Email</label><br/>
-      <input style={{width:'400px',height:50,padding:20,margin:5,border:"1px solid grey",borderRadius:'10px'}}   name='email' type="email" placeholder="Email Address"  /> <br />
-      <label style={{paddingLeft:10,marginTop:20,fontSize:'20px',fontWeight:'400'}} >Password</label><br/>
-      <input style={{width:'400px',height:50,padding:20,margin:5,border:"1px solid grey",borderRadius:'10px'}}   name='password' type="password" placeholder="Password"  /> <br />
-      <button style={{backgroundColor:'#3b82f6',borderRadius:10,marginTop:12,float:'right',color:"#fff",border:"1px solid blue",padding:"8px 16px",fontSize:'18px',fontWeight:500,}}>Sign In</button>
+          )}
+          <img
+            src={LoginLogo}
+            className={`h-30 w-40 ${logoLoaded ? 'block' : 'hidden'}`}
+            alt="Login Logo"
+            onLoad={handleLogoLoad}
+          />
         </div>
-        <p style={{paddingLeft:50,fontSize:'14px',fontWeight:'600',marginTop:200}}>You can not access your account?<span style={{fontSize:'18px',fontWeight:'600',color:'blue'}}> Recover account </span></p>
+        <div className="mt-6">
+          <label className="text-gray-600 text-sm font-semibold">Email</label>
+          <input
+            className="w-full h-10 mt-2 px-3 py-2 border border-gray-300 rounded"
+            name="email"
+            type="email"
+            placeholder="Email Address"
+          />
+          <label className="text-gray-600 text-sm font-semibold mt-4">Password</label>
+          <div className="relative flex items-center">
+            <input
+              className="w-full h-10 mt-2 px-3 py-2 border border-gray-300 rounded pr-10"
+              name="password"
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+            />
+            <button
+              className="absolute top-0 right-0 h-full text-gray-500 focus:outline-none flex items-center px-2 mt-1"
+              onClick={togglePasswordVisibility}
+            >
+              <FontAwesomeIcon
+                icon={showPassword ? faEyeSlash : faEye}
+                className="text-gray-600"
+              />
+            </button>
+          </div>
+          <button
+            className="bg-blue-500 hover:bg-blue-600 text-white rounded mt-6 py-2 px-4 font-semibold"
+          >
+            Sign In
+          </button>
         </div>
-        
-        </div>
-    );
-}
-export default Workshopage;
+        <p className="mt-8 text-sm text-gray-600 font-semibold">
+          Can't access your account?{" "}
+          <span className="text-blue-500 font-semibold">Recover account</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default WorkshopPage;
